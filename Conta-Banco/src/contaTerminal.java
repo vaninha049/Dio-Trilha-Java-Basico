f2a12bd18018614924e42f8b1698e84201845931
Vanessa Vieira <vaninha049@hotmail.com>
2023-09-10 23:03:34 -0300

    Projeto Conta Terminal finalizado

--- Conta-Banco/src/contaTerminal.java
+++ Conta-Banco/src/contaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class contaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  
+    public static void main(String[] args) {
+        // criando o objeto scanner
+        double saldo = 237.48;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+       
+        System.out.println("ola, Digite seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite o numero da conta ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o numero da agencia");
+        String agencia = scanner.next();
+        
+        
+        
+        System.out.println("Ola," + nome + " Obrigado por criar uma conta em nosso banco, sua agencia e " + agencia + " conta " + numero + " e seu saldo " + saldo + " que ja esta disponivel para saque ");
+        scanner.close();
     }
 }
